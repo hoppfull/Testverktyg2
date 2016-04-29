70c89ad9d7913fbe40acba354d646c9e624c7725
Björn Kihlberg <hoppfull@gmail.com>
2016-04-29 09:36:16 +0200

    initialized demo01 and demo02 projects

---
+++ Demo01 - WPF window spawning/App.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Demo01___WPF_window_spawning {
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application {
+    }
+}
