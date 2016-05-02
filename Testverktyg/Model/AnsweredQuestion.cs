ff9102d28c3dacd5fd61838c1a66893650f8edc4
Björn Kihlberg <hoppfull@gmail.com>
2016-05-02 11:36:08 +0200

    intialized model in class library

--- Testverktyg/Model/AnsweredQuestion.cs
+++ Testverktyg/Model/AnsweredQuestion.cs
@@ -4,9 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Testverktyg {
-    public class MyTable {
-        public int ID { get; set; }
-        public string Text { get; set; }
+namespace Testverktyg.Model {
+    class AnsweredQuestion {
     }
 }
--- Testverktyg/MyDatabase.cs
+++
@@ -1,7 +0,0 @@
-﻿using System.Data.Entity;
-
-namespace Testverktyg {
-    public class MyDatabase : DbContext {
-        public DbSet<MyTable> MyTables { get; set; }
-    }
-}
