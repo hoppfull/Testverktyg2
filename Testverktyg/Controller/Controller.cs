006098e2bc70e6928c1de7a6eb4289b26e609d5e
Björn Kihlberg <hoppfull@gmail.com>
2016-05-03 13:47:40 +0200

    whatevs

--- Testverktyg/Controller/Controller.cs
+++ Testverktyg/Controller/Controller.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Testverktyg.Model;
+using Testverktyg.Context;
 
 namespace Testverktyg.Controller {
     static public class Controller {
@@ -63,14 +64,11 @@ namespace Testverktyg.Controller {
         }
 
         public static bool IsEmailValid(string email) {
-            return true;
+                return true;
         }
 
-        public static bool EmailExists(string email) {
-            return true;
-        }
-
-        public static bool UpdateEmail(string email) {
+        public static bool UpdateEmail(StudentAccount selectedStudentAccount, string email) {
+            // hint: mylist.Exists(x => x.Email == email);
             return true;
         }
 
@@ -78,44 +76,11 @@ namespace Testverktyg.Controller {
             return true;
         }
 
-        public static string ResetPassword(AbstractUser user) {
-            return null;
-        }
-
-        public static string GenerateNewPassword() {
-            return null;
-        }
-
         public static bool UpdatePassword(string password) {
             return true;
         }
 
         public static bool IsUserEmailValid(string email) {
-            return true;
-        }
-
-        public static AbstractUser CreateUser(string name, string email, UserType userType) {
-            // return null if operation fails
-            // client code must check if result is null
-            return null;
-        }
-
-        public static Subject CreateSubject(string name) {
-            return null;
-        }
-
-        public static bool SubjectExists(string name) {
-            return true;
-        }
-
-        public static bool IsSubjectValid(string name) {
-            return true;
-        }
-
-        public static bool DeleteUser(AbstractUser user) {
-            // if last admin, don't delete else, remove from DB
-            // if student has testforms, only set IsNotRemoved = false, else delete from DB
-            // if teacher has testdefinitions, only set IsNotRemoved = false, else delete from DB
             return true;
         }
     }
