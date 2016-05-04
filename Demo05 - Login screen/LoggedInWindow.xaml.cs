35a618917b86d1477b2eccbb35d25873fb24f8d3
Björn Kihlberg <hoppfull@gmail.com>
2016-05-04 09:31:48 +0200

    implemented Demo05

---
+++ Demo05 - Login screen/LoggedInWindow.xaml.cs
@@ -0,0 +1,10 @@
+﻿using System.Windows;
+
+namespace Demo05___Login_screen {
+    public partial class LoggedInWindow : Window {
+        public LoggedInWindow() {
+            InitializeComponent();
+            WindowStartupLocation = WindowStartupLocation.CenterScreen;
+        }
+    }
+}
---
+++ Demo05 - Login screen/MainWindow.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+
+namespace Demo05___Login_screen {
+    public partial class MainWindow : Window {
+        public MainWindow() {
+            InitializeComponent();
+            WindowStartupLocation = WindowStartupLocation.CenterScreen;
+        }
+
+        private void btn_Login_Click(object sender, RoutedEventArgs e) {
+            LoggedInWindow w = new LoggedInWindow();
+            w.Show();
+            Close();
+        }
+    }
+}
