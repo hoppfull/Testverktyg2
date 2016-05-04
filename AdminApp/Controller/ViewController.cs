c45aba72e5b6e8f786d843919f3c5d750360c09d
Björn Kihlberg <hoppfull@gmail.com>
2016-05-04 12:04:03 +0200

    implemented basic logout functionality

---
+++ AdminApp/Controller/ViewController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace AdminApp.Controller {
+    class ViewController {
+
+        public void Logout(Window window) {
+            MainWindow loginscreen = new MainWindow();
+            loginscreen.Show();
+            window.Close();
+        }
+    }
+}
