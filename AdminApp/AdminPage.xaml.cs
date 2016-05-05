1b10f3df2e88695dc8a8eb3af19b9db5f5bc577c
Björn Kihlberg <hoppfull@gmail.com>
2016-05-05 16:04:44 +0200

    implemented skeleton UI to users tab on admin page

--- AdminApp/AdminPage.xaml.cs
+++ AdminApp/AdminPage.xaml.cs
@@ -12,8 +12,12 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 using Testverktyg.Model;
+using Testverktyg.Repository;
 
 namespace AdminApp {
+    public enum UserType {
+        Admin, Teacher, Student
+    }
     public partial class AdminPage : Window {
         public AdminAccount UserAccount { get; }
         public AdminPage(AdminAccount userAccount) {
@@ -22,23 +26,55 @@ namespace AdminApp {
             UserAccount = userAccount;
         }
 
-        private void btn_EditSubject_Click(object sender, RoutedEventArgs e)
-        {
+        private void btn_EditSubject_Click(object sender, RoutedEventArgs e) {
 
         }
 
-        private void btn_RemoveSubject_Click(object sender, RoutedEventArgs e)
-        {
+        private void btn_RemoveSubject_Click(object sender, RoutedEventArgs e) {
 
         }
 
-        private void btn_AddSubject_Click(object sender, RoutedEventArgs e)
-        {
+        private void btn_AddSubject_Click(object sender, RoutedEventArgs e) {
 
         }
 
-        private void btn_InspectTestDefinition_Click(object sender, RoutedEventArgs e)
-        {
+        private void btn_InspectTestDefinition_Click(object sender, RoutedEventArgs e) {
+
+        }
+
+        private void cbx_SelectUserType_SelectionChanged(object sender, SelectionChangedEventArgs e) {
+            switch ((UserType)((ComboBoxItem)cbx_SelectUserType.SelectedItem).Tag) {
+                case UserType.Admin:
+                    lvw_Users.ItemsSource = Repository<AdminAccount>.Instance.GetAll();
+                    break;
+                case UserType.Teacher:
+                    lvw_Users.ItemsSource = Repository<TeacherAccount>.Instance.GetAll();
+                    break;
+                case UserType.Student:
+                    lvw_Users.ItemsSource = Repository<StudentAccount>.Instance.GetAll();
+                    break;
+            }
+        }
+
+        private void lvw_Users_SelectionChanged(object sender, SelectionChangedEventArgs e) {
+
+        }
+
+        private void btn_AddUser_Click(object sender, RoutedEventArgs e) {
+            btn_AddUser.Visibility = Visibility.Collapsed;
+            skp_SaveUser.Visibility = Visibility.Visible;
+        }
+
+        private void btn_SaveUser_Click(object sender, RoutedEventArgs e) {
+            btn_AddUser.Visibility = Visibility.Visible;
+            skp_SaveUser.Visibility = Visibility.Collapsed;
+        }
+
+        private void btn_RemoveUser_Click(object sender, RoutedEventArgs e) {
+            
+        }
+
+        private void btn_ResetUserPassword_Click(object sender, RoutedEventArgs e) {
 
         }
     }
