3cd90bb00284e6ba1cbef75519fad461f002ed4b
Björn Kihlberg <hoppfull@gmail.com>
2016-05-06 20:19:41 +0200

    added foreign key definition for subject to TestDefinition model

--- AdminApp/MainWindow.xaml.cs
+++ AdminApp/MainWindow.xaml.cs
@@ -1,36 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data.Entity;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
 using Testverktyg;
-using Testverktyg.Model;
-using Testverktyg.Context;
 
 namespace AdminApp {
     public partial class MainWindow : Window {
         public MainWindow() {
             InitializeComponent();
-            Console.WriteLine("hello world!");
-
-            //using (var db = new TestverktygContext()) {
-            //    db.AdminAccounts.Add(new AdminAccount("Kalle", "mail", "abs"));
-            //    db.StudentAccounts.Add(new StudentAccount("a", "b", "c"));
-            //    db.TeacherAccounts.Add(new TeacherAccount("x", "y", "z"));
-            //    db.TeacherAccounts.Add(new TeacherAccount("u", "v", "w"));
-
-            //    db.SaveChanges();
-            //}
             MockData.Reset();
         }
     }
--- Testverktyg/Model/TestDefinition.cs
+++ Testverktyg/Model/TestDefinition.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Testverktyg.Model {
     public enum TestDefinitionState {
@@ -11,10 +12,18 @@ namespace Testverktyg.Model {
         public string Title { get; set; }
         public string Paragraph { get; set; }
         public int MaxScore { get; set; }
-        public Subject Subject { get; set; }
         public TestDefinitionState TestDefinitionState { get; set; }
         public bool IsNotRemoved { get; set; }
         public IList<Question> Questions { get; set; }
+        
+        //Foreign keys:
+        public int SubjectId { get; set; }
+        [ForeignKey("SubjectId")]
+        public Subject Subject { get; set; }
+        public int TeacherAccountId { get; set; }
+        [ForeignKey("TeacherAccountId")]
+        public TeacherAccount TeacherAccount { get; set; }
+        
         public TestDefinition() {
             TestDefinitionState = TestDefinitionState.Created;
             IsNotRemoved = true;
