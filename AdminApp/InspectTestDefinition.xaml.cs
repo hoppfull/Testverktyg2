b9e7a6a1fd5a10231a7a87b327518106abce8d4a
Björn Kihlberg <hoppfull@gmail.com>
2016-05-10 15:23:17 +0200

    'solved' graphical bug when disabling admin window

--- AdminApp/InspectTestDefinition.xaml.cs
+++ AdminApp/InspectTestDefinition.xaml.cs
@@ -8,10 +8,15 @@ using Testverktyg.Repository;
 namespace AdminApp {
     public partial class InspectTestDefinition : Window {
         private TestDefinition TestDefinition { get; }
-        public InspectTestDefinition(TestDefinition testDefinition) {
+        private AdminPage Parent { get; }
+        public InspectTestDefinition(AdminPage parent, TestDefinition testDefinition) {
             InitializeComponent();
             WindowStartupLocation = WindowStartupLocation.CenterScreen;
             TestDefinition = testDefinition;
+            Parent = parent;
+            Parent.IsEnabled = false;
+            // TODO: Find better solution:
+            parent.Opacity = 0;
         }
 
         private void btn_ValidateTestDefinition_Click(object sender, RoutedEventArgs e) {
@@ -22,6 +27,9 @@ namespace AdminApp {
                     int.Parse((string)((ComboBoxItem)cbx_TimeLimit.SelectedItem).Content),
                     dpk_FinalDate.SelectedDate.Value);
             }
+
+            MessageBox.Show("Provet har godkänts!");
+            Close();
         }
 
         private void cbx_TimeLimit_SelectionChanged(object sender, SelectionChangedEventArgs e) {
@@ -30,6 +38,12 @@ namespace AdminApp {
 
         private void dpk_FinalDate_SelectedDateChanged(object sender, SelectionChangedEventArgs e) {
             btn_ValidateTestDefinition.IsEnabled = cbx_TimeLimit.SelectedIndex != -1 && dpk_FinalDate.SelectedDate.HasValue;
+        }
+
+        private void win_InspectTestDefinition_Closing(object sender, System.ComponentModel.CancelEventArgs e) {
+            Parent.IsEnabled = true;
+            // TODO: Find better solution:
+            Parent.Opacity = 1;
         }
     }
 }
